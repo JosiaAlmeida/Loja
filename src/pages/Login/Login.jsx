94b1f228ae26ecb8c30b53e29f178068ec0c826c
Josia Almeida <josiaalmeida0001@gmail.com>
2021-04-01 19:21:22 +0100

    Tela Login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
 import './login.css'
-import {Form, Button, Input, FormGroup, Label} from 'reactstrap';
+import {Form, Button, Input, FormGroup, Label, Col, Row} from 'reactstrap';
 export default ()=>
     <div id="Principal">
         <Form className="Login">
-            <FormGroup>
-                <Input type="text" name="nome" placeholder="Digite o nome" />
-            </FormGroup>
+            <Row form>
+                <Col md="6">
+                    <FormGroup>
+                        <Input type="text" name="nome" placeholder="Nome" className="input"/>
+                    </FormGroup>
+                </Col>
+                <Col md="6">
+                    <FormGroup>
+                        <Input type="text" name="apelido" placeholder="Apelido" className="input" />
+                    </FormGroup>
+                </Col>
+                <Col md="12">
+                    <FormGroup>
+                        <Input type="text" name="email" placeholder="Email" className="input" />
+                    </FormGroup>
+                </Col>
+                <Col md="12">
+                    <FormGroup>
+                        <Input type="number" name="idade" placeholder="Idade" className="input" />
+                    </FormGroup>
+                </Col>
+                <Col md="12">
+                    <Button type="submit" block>
+                        Login
+                    </Button>
+                </Col>
+            </Row>
         </Form>
     </div>
